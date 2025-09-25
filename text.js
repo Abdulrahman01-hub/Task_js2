f03e8ff53c1690d2aa76e7a68d147422b356b335
Abdulrahman01-hub <barbarossa.123456.1@gmail.com>
2025-09-26 00:09:52 +0400

    task

--- text.js
+++ text.js
@@ -78,6 +78,31 @@
 // friends.shift();
 // console.log(friends);
 
+//task 7
+// let numbers = [1,2,3,4,5,6,7,8,9,10];
+// let first = numbers[0];
+// let second = numbers[1];
+// let rest = numbers.slice(2);
+
+// console.log("birinci", first);
+// console.log("ikinci", second);
+// console.log("qalan massiv", rest);
+
+//task 8
+// let arr = [12, 45, 7, 89, 23, 56, 91, 34, 67, 10];
+// let max = Math.max(...arr);
+// console.log("maksimum deyer", max);
+
+//task 9
+// let numbers = [2, 4, 6, 8, 10];
+// let copy = numbers.slice();
+
+// for (let i = 0; i < copy.length; i++) {
+//     copy[i] = copy[i] * 2;
+// }
+// console.log("orijinal massiv", numbers);
+// console.log("ikiqat artirilmis kopya", copy);
+
 //task 10
 // let employees = [
 //     [ 'Jaylee Macy', 'marketing' ],
@@ -105,7 +130,8 @@
 //         console.log("novbede: " + musteri);
 //     } else {
 //         musteri.push(text);
-//         console.log(musteri); 
+//         console.log(musteri);
+//         console.log(musteri);
 //     }
 // }
 // users("Qafar Qemberzade");
@@ -146,3 +172,78 @@
 // }
 // let arr = [5, -3, 8, -1, 0, 12, -7];
 // positive(arr);
+
+//task 16
+// function kebabToUpper(text) {
+//     let parts = text.split("-");
+//     let upper = parts.map(word => word.toUpperCase());
+//     return upper.join("_");
+// }
+// console.log(kebabToUpper('first-user'));
+// console.log(kebabToUpper('my-super-project'));
+
+//task 17
+// let numbers = [1, 5, -7, 3, -9, 4, -6, 2];
+
+// let newArr = numbers.map(num => {
+//     if (num < 0) {
+//         return Math.abs(num);
+//     } else {
+//         return num * 2;
+//     }
+// });
+// console.log("orijinal massiv", numbers);
+// console.log("yeni massiv", newArr);
+
+//task 18
+// function ThreeChars(arr) {
+//     let result = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         result.push(arr[i].slice(0, 3));
+//     }
+//     return result;
+// }
+// let words = ["Salam", "Dunya", "Python", "JavaScript"];
+// console.log(ThreeChars(words));
+
+//task 19
+// let names = ["Abdul", "Qafar", "Amir", "Fuad", "Altay", "Nilufer"];
+
+// let filtered = [];
+// for (let i = 0; i < names.length; i++) {
+//     if (names[i][0] === "A") {
+//         filtered.push(names[i]);
+//     }
+// }
+// console.log("A ile baslayanlar", filtered);
+
+//task 20
+// let price = [100, 200, 50, 80, 120];
+
+// function addTax(arr) {
+//     let result = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         result.push(arr[i] * 1.2);
+//     }
+//     return result;
+// }
+// let finalPrices = addTax(price);
+// console.log("ƏDV ile qiymetler", finalPrices);
+
+//task 21
+// function countEvensAndOdds(arr) {
+//     let text = 0;
+//     let text2 = 0;
+
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] % 2 === 0) {
+//             text++;
+//         } else {
+//             text2++;
+//         }
+//     }
+
+//     return `massivde ${text} cut və ${text2} tek eded var`;
+// }
+// let numbers = [1, 2, 3, 4, 6, 8];
+// console.log(countEvensAndOdds(numbers));
